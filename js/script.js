fddd37d5233762bd241e50e6d4441afab9339c87
TheAnalystAM <theanalystam@gmail.com>
2025-11-12 21:57:38 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -46,7 +46,7 @@ $(document).ready(function(){ // begin document.ready block
    
    $('#thelateststories2').click(function(){
       
-        window.location.href = "/stories/economics/evtariffs";
+        window.location.href = "/stories/markets/palantirvalue";
       
    });
    
