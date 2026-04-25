2c630f1c6032ac540da0de777aeb41d9f10d3b83
TheAnalystAM <theanalystam@gmail.com>
2026-04-25 12:49:27 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,21 +8,19 @@ $(document).ready(function(){ // begin document.ready block
 
    // CAPSTONE
    
-   <script>
-     const video = document.getElementById("myVideo");
+   const video = document.getElementById("myVideo");
 
-     video.addEventListener("click", function () {
-       if (video.paused) {
+   video.addEventListener("click", function () {
+      if (video.paused) {
        video.play();
-    }
-  });
+      }
+    });
 
   // Loop automatically after it finishes
   video.addEventListener("ended", function () {
     video.currentTime = 0;
     video.play();
   });
-  </script>
 
    // NEWS TEASER
    
